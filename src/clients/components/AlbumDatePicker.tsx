07ee988582fda89ec0b5b6b12ab017304d4bc377
jindorry <ekfaktldk11@gmail.com>
2024-04-30 20:34:12 +0900

    tmp

--- src/clients/components/AlbumDatePicker.tsx
+++ src/clients/components/AlbumDatePicker.tsx
@@ -1,10 +1,18 @@
 import React, { ReactElement, useState } from 'react';
 import DatePicker from "react-datepicker";
+import { getAllBucketImages } from '../api/image';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function AlbumDatePicker(): ReactElement {
     const [startDate, setStartDate] = useState(new Date());
     return (
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date ?? startDate)} />
+        <DatePicker selected={startDate} onChange={(date) => onStartDateChange(date ?? startDate)} />
     );
+
+    async function onStartDateChange(date: Date): Promise<void> {
+        if (!date || startDate === date) return;
+        setStartDate(date);
+
+        await getAllBucketImages(date);
+    }
 }
