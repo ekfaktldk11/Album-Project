544d51feeafb9adc94d8c11a735d92b8b3ca1889
jindorry <ekfaktldk11@gmail.com>
2024-05-15 13:24:19 +0900

    alt 속성 수정

--- src/clients/components/imgContainer.tsx
+++ src/clients/components/imgContainer.tsx
@@ -3,5 +3,5 @@
 import React, { ReactElement } from 'react';
 
 export default function ImgContainer(base64ImageUrl: string): ReactElement {
-    return <img src={base64ImageUrl} alt="loaded imgs selected on datePicker" />;
+    return <img src={base64ImageUrl} alt="A loaded img selected on datePicker" />;
 }
