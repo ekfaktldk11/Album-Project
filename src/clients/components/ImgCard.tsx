598543b180249adab6e398432abe32a9853c9d86
jindorry <ekfaktldk11@gmail.com>
2024-05-18 17:52:38 +0900

    temporary styling

--- src/clients/components/ImgCard.tsx
+++ src/clients/components/ImgCard.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from 'react';
+import "../styles/temp.css";
 
 const ImgCard = ({base64ImageUrl}: { base64ImageUrl: string }): ReactElement => (
-    <img src={base64ImageUrl} alt="A loaded img selected on datePicker" />
+    <img className='imgCard' src={base64ImageUrl} alt="A loaded img selected on datePicker" />
 );
 
 export default ImgCard;
