6caa3777d19c450f62e08a75e9e6bdad987436c5
jindorry <ekfaktldk11@gmail.com>
2025-02-09 18:40:18 +0900

    delete unused files

--- src/clients/components/ImgCard.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from 'react';
-import "../styles/temp.css";
-
-const ImgCard = ({base64ImageUrl}: { base64ImageUrl: string }): ReactElement => (
-    <img className='imgCard' src={base64ImageUrl} alt="A loaded img selected on datePicker" />
-);
-
-export default ImgCard;
