ae840c1bf150a082d9ee11d1a314035f88eecea4
jindorry <ekfaktldk11@gmail.com>
2025-02-09 18:40:18 +0900

    delete unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import AlbumContentContainer from './clients/components/AlbumContentContainer';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <AlbumContentContainer />
     </div>
   );
 }
