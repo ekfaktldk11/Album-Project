5e00a359dbcc3f7ca77ed6642d7c6da45d0b373d
jindorry <ekfaktldk11@gmail.com>
2025-02-11 22:02:55 +0900

    trivial navbar style modification

--- src/clients/svg/drawer.tsx
+++ src/clients/svg/drawer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 const SVG_DRAWER: React.FC = () => (
-  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg width={48} height={48} viewBox="-2 -2 24 24" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M4 6H14M4 10H14M4 14H14"
       stroke="#000000"
