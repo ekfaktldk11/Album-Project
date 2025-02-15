9590d2d3f88e9d5c5d67181bdac4a2fa4bdf89f9
ekfaktldk11 <ekfaktldk11@gmail.com>
2025-02-15 18:58:27 +0900

    temp Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./clients/components/Navbar";
+import PageContainer from "./clients/components/PageContainer";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <PageContainer />
     </div>
   );
 }
---
+++ src/clients/components/PageContainer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function PageContainer() {
+  return <Container>Container</Container>;
+}
